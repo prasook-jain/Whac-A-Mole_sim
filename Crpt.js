ddc9f5fd07deacc50a382ddd41ff5a99d6d644e4
PrasukDJain <prasookjain@gmail.com>
2015-11-21 03:34:55 +0530

    Less GUI based whac-a-mole simulation Game

---
+++ Crpt.js
@@ -0,0 +1,86 @@
+/*var run = function() {
+	var count = 0;
+	var blockId = parseInt(Math.random() * 9 + 1);
+	var str = String(blockId);
+	//console.log(str);
+	var block = document.getElementById(str);
+
+	block.style.backgroundColor = "red";
+	//console.log(String(blockId));
+}
+*/
+function getBlockId() {
+	return parseInt(Math.random() * 9 + 1);
+}
+
+function assignBlock(preBlockId) {
+	var blockId = getBlockId();
+
+	if (preBlockId === blockId)
+		blockId = getBlockId();
+	//restrict from choose same block
+
+	var block = document.getElementById(String(blockId));
+	block.style.backgroundColor = 'red';
+	return block;
+}
+
+function resetBlock(block) {
+	block.style.backgroundColor = 'black';
+	return parseInt(block.id);
+}
+
+var getSec = function() {
+	var today = new Date();
+	return today.getSeconds();
+}
+
+var life = 2;   //no of lives
+var score = 0;  //scores
+var preBlockId = -1;  //previously assigned blocks
+var gameSpeed = 2000;  //gamespeed intially
+var block;
+var flag = false;
+var breakTime;
+var name = prompt('hey just enter your name ','Prasook Jain');
+
+
+var runV2 = function() {
+	/*
+    Added following feature than previous version:-
+      1. Lifes
+      2. Score
+      3. No same block selection consecutivly
+    */
+	life = 2;
+	score = 0;
+	runGame();
+}
+
+function runGame() {
+		if(life<0) {
+			exitFunc();
+			return;
+		}
+		block = assignBlock(preBlockId);
+		flag = false;
+		breakTime = setTimeout(function (){ updateGame(); runGame();},gameSpeed);
+}
+
+function updateGame() {
+	if(life<0) {
+		clearTimeout(breakTime);
+	}
+	else if(flag == true) score++;
+	else if(life>=0) life--;
+
+	preBlockId = resetBlock(block);
+}
+
+function clickedBlock(elem){
+	if(elem.id == block.id) flag = true;
+}
+
+function exitFunc(){
+	alert('Hey '+name+'!, Your score is '+score);
+	}
